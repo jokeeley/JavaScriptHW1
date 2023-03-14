77bbee3da1f20f003825f1e58f89eca1e0d088a4
Jo Keeley <student@Souths-MBP-7.lan>
2023-03-13 19:53:48 -0500

    initial hw assignment

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1
+console.log(28);
+
+// 2
+// Numbers, Strings, Booleans, Null, Undefined
+
+// 3
+// + - * ** % ()
+
+// 4
+console.log(1 % 2);
+console.log(1 % -2);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Hawk" + "Eye");
+
+// 7
+console.log("Josephine Keeley".length);
+
+// 8
+console.log("Avengers: Endgame"[2]);
