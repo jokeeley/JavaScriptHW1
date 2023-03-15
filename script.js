c44b76bfebe04bb6d7b0fedd2c2113cebd3c1ff0
Jo Keeley <student@Souths-MBP-7.lan>
2023-03-15 09:24:37 -0500

    updated answers

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ console.log(28);
 // + - * ** % ()
 
 // 4
-console.log(1 % 2);
-console.log(1 % -2);
+console.log(5 % 2);
+console.log(5 % 4);
 
 // 5
 console.log('I\'M MARY POPPINS, Y\'ALL!');
@@ -18,7 +18,33 @@ console.log('I\'M MARY POPPINS, Y\'ALL!');
 console.log("Hawk" + "Eye");
 
 // 7
-console.log("Josephine Keeley".length);
+console.log("Josephine".length + "Keeley".length);
 
 // 8
-console.log("Avengers: Endgame"[2]);
+console.log("Avengers: Endgame"[16]);
+
+// BONUS 
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log((("I".length + "Am".length + "Iron".length +  "Man".length)));
+
+// 4
+console.log("CAP"[1]); 
+console.log("VISION"[0]); 
+console.log("OKOYE"[4]); 
+console.log("NICK FURY"[0]); 
+console.log("GROOT"[0]); 
+console.log("DOCTOR STRANGE"[13]); 
+console.log("THOR"[3]); 
+console.log("STAR-LORD"[0]); 
+
+// 5
+var m=25;
+var c=4;
+var u = m + c
+console.log(u);
